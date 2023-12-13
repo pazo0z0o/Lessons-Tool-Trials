0225889bdc7a20b5f786b833c9dc0f6dc7a08820
Kosmas_Stamos <kossmasstamoss@gmail.com>
2023-12-13 13:39:05 +0200

    first push

---
+++ RabbitMQ lesson/RabbitSender/RabbitReceiver2/Program.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Same proccess as the sender
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Receiver2 App";
+
+
+IConnection con = factory.CreateConnection();
+//RabbitMQ model
+IModel channel = con.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-key";
+string queName = "DemoQueue";
+
+//Declare an exchange
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+//Declare a queue and bind an exchange to it
+channel.QueueDeclare(queName, false, false, false, null);
+channel.QueueBind(queName, exchangeName, routingKey, null);
+
+//Set the Quality of Service
+channel.BasicQos(prefetchSize: 0, 1, false);
+
+//Setup a consumer of the message. Has to also be decoded from its byte[] form
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, args) =>
+{
+    Task.Delay(TimeSpan.FromSeconds(3)).Wait();
+    var body = args.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Message Received: {message}");
+    channel.BasicAck(args.DeliveryTag, false);
+
+};
+
+string consumerTag = channel.BasicConsume(queName, false, consumer);
+
+Console.ReadLine();
+
+channel.BasicCancel(consumerTag);
+
+channel.Close();
+con.Close();
+
+
+
+
+
+
---
+++ RabbitMQ lesson/RabbitSender/RabbitSender/Program.cs
@@ -0,0 +1,61 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Sender App";
+
+
+IConnection con = factory.CreateConnection();
+//RabbitMQ model
+IModel channel = con.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-key";
+string queName = "DemoQueue";
+
+//Declare an exchange
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+//Declare a queue and bind an exchange to it
+channel.QueueDeclare(queName,false,false,false,null);
+channel.QueueBind(queName,exchangeName,routingKey,null);
+
+//Publish a simple message
+//byte[] messageBodyInBytes = Encoding.UTF8.GetBytes("Hello there!");
+//channel.BasicPublish(exchangeName, routingKey, false, null, messageBodyInBytes);
+
+//Publish multiple messages to test the queue
+for (int i = 0; i < 60; i++)
+{
+    Console.WriteLine($"Sending message number {i} !!");
+    byte[] messageBodyInBytes = Encoding.UTF8.GetBytes($"Message #{i}");
+    channel.BasicPublish(exchangeName, routingKey, false, null, messageBodyInBytes);
+    //The messages should not come at the same time
+    Thread.Sleep(1000);
+}
+
+Console.ReadLine();
+channel.Close();
+con.Close();        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
